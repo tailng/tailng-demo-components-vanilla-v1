69b5947ca842d35c2255b3d286765114e6fb65eb
Prince Francis <princekfrancis@gmail.com>
2026-03-31 18:27:31 +0530

    checkbox examples added.

--- src/app/playground/form/checkbox/checkbox.component.ts
+++ src/app/playground/form/checkbox/checkbox.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TngCheckboxComponent } from '@tailng-ui/components';
 
 @Component({
   selector: 'app-checkbox',
   standalone: true,
   templateUrl: './checkbox.component.html',
   styleUrl: './checkbox.component.css',
+  imports: [TngCheckboxComponent],
 })
 export class CheckboxComponent {}
