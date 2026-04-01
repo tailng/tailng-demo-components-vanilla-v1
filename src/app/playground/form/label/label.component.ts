51cd1103796bd32d08a248ffe00c16455fd84fa0
Prince Francis <princekfrancis@gmail.com>
2026-04-01 11:12:36 +0530

    label demo added.

--- src/app/playground/form/label/label.component.ts
+++ src/app/playground/form/label/label.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TngLabelComponent } from '@tailng-ui/components';
 
 @Component({
   selector: 'app-label',
   standalone: true,
   templateUrl: './label.component.html',
   styleUrl: './label.component.css',
+  imports: [TngLabelComponent],
 })
 export class LabelComponent {}
