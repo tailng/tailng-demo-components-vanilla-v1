b23a3d47d4dd874a2487a38bcc933ae82a2cdbc4
Adarsh_E <adarshedathodathil@gmail.com>
2026-04-22 15:57:30 +0530

    Style:Examble change.

--- src/app/playground/form/label/label.component.ts
+++ src/app/playground/form/label/label.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { TngLabelComponent } from '@tailng-ui/components';
+import { TngLabelComponent, TngInputComponent } from '@tailng-ui/components';
 
 @Component({
   selector: 'app-label',
   standalone: true,
   templateUrl: './label.component.html',
   styleUrl: './label.component.css',
-  imports: [TngLabelComponent],
+  imports: [TngLabelComponent, TngInputComponent],
 })
 export class LabelComponent {}
