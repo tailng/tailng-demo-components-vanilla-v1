f311cd1c498c815f57acec790c9650a925a3d90b
Adarsh_E <adarshedathodathil@gmail.com>
2026-04-28 12:28:59 +0530

    Style:Examples Updated.

--- src/app/playground/layout/card/card.component.ts
+++ src/app/playground/layout/card/card.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
-
+import { TngCardActionsComponent, TngCardComponent, TngCardContentComponent, TngCardDescriptionComponent, TngCardFooterComponent, TngCardHeaderComponent, TngCardTitleComponent, TngCardMediaComponent, TngCardDividerComponent, TngCardLinkComponent } from '@tailng-ui/components';
 @Component({
-  selector: 'app-card',
+  selector: 'app-card-overview-plain-css',
   standalone: true,
+  imports: [
+    TngCardComponent,
+    TngCardHeaderComponent,
+    TngCardTitleComponent,
+    TngCardDescriptionComponent,
+    TngCardContentComponent,
+    TngCardFooterComponent,
+    TngCardActionsComponent,
+    TngCardMediaComponent,
+    TngCardDividerComponent,
+    TngCardLinkComponent
+],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
